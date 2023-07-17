087abbe55defb32f226a9cca37d795be9715c783
Devsed-official <devsedofficial@gmail.com>
2023-07-18 00:30:24 +0530

    some

--- app/components/navlink.js
+++ app/components/navlink.js
@@ -5,18 +5,22 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 
-const Navlink = ({ text, link,classes }) => {
+const Navlink = ({ text, link, classes }) => {
   const path = usePathname();
   const pathWithoutLang = path.replace(/^\/[a-z]{2}/, "");
+  console.log(pathWithoutLang)
   return (
     <Link
       href={link}
-      className={clsx(classes,"relative font-gilroy font-[350] text-[14px] text-[#C7C7C7] hover:text-white")}
+      className={clsx(
+        classes,
+        "relative font-gilroy font-[350] text-[14px] text-[#C7C7C7] hover:text-white"
+      )}
     >
       {link === pathWithoutLang && (
         <motion.span
           layoutId="underline"
-          className="absolute left-0 top-full block h-[1px] w-full bg-white"
+          className="absolute left-0 top-5 block h-[1px] w-full bg-white"
         />
       )}
       {text}
