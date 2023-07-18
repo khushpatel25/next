03d2bfcb4a2d7219ce5b4c0b4dddabb2c4dc6712
Devsed-official <devsedofficial@gmail.com>
2023-07-19 00:49:37 +0530

    some more fixes

--- app/sections/home/hero.js
+++ app/sections/home/hero.js
@@ -10,10 +10,10 @@ const Hero = () => {
   return (
    
       <Padding classes={'bg-brandblack'}>
-        <div className="font-gilroy flex flex-col justify-center text-center items-center text-white text-[2.2rem] font-medium lg:text-[3rem] 2xl:text-[6rem] gap-4 lg:gap-9 min-h-[120vh] sm:min-h-[150vh]  lg:min-h-[180vh] xl:min-h-[180vh] 2xl:min-h-[150vh] leading-1">
-          <div className="flex flex-col text-center items-center gap-2">
+        <div className="font-gilroy flex flex-col justify-center text-center items-center text-white text-[2.2rem] font-medium lg:text-[3rem] 2xl:text-[6rem] gap-4 lg:gap-9 min-h-[120vh] sm:min-h-[150vh]  lg:min-h-[160vh] xl:min-h-[180vh] 2xl:min-h-[150vh] leading-1">
+          <div className="flex flex-col text-center items-center gap-2 lg:pt-20 xl:pt-0 ">
             <div>Caring for your home made easy.</div>
-            <div className="text-brandgrey text-[0.9rem] lg:text-[1.2rem] 2xl:text-[1.5rem]  flex font-normal max-w-[80vw] lg:max-w-[55vw] leading-8">
+            <div className="text-brandgrey font-silka text-[0.9rem] lg:text-[1.2rem] 2xl:text-[1.5rem]  flex font-light max-w-[80vw] lg:max-w-[55vw] leading-8">
               Download our App and Book services, see prices, read reviews and
               chat with pros, all in the app.
             </div>
